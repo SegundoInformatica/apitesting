052554464754cd27b66511ca74d11bab061ab63f
André Luis <andrescalisejr@gmail.com>
2023-05-15 20:07:52 -0300

    change route

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	@cd ./api && ./run.sh
--- api/serve.py
+++ api/serve.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route("/try", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def _():
     name = request.args.get("name") or request.form.get("name") or "World"
     return jsonify(f"Hello, {name}!")
