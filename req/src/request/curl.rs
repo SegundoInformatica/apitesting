2cd080bdf72fba52e6954528e9449ab7ed35d5f3
André Luis (devkcud) <andrescalisejr@gmail.com>
2023-05-20 19:18:21 -0300

    fix: curl duplicating requests

--- req/src/request/curl.rs
+++ req/src/request/curl.rs
@@ -3,33 +3,31 @@ use std::{io, process::Command};
 pub struct CURL {
     data: String,
     endpoint: String,
-    curl_command: Command,
 }
 
 impl CURL {
     pub fn new(endpoint: String, data: String) -> Self {
-        let command = Command::new("curl");
-
         return Self {
             data,
             endpoint,
-            curl_command: command,
         };
     }
 
     pub fn get(&mut self) -> Result<String, io::Error> {
-        self.curl_command.args(["-X", "GET", &self.endpoint, "-d", &self.data]);
+        let mut command = Command::new("curl");
+        command.args(["-X", "GET", &self.endpoint, "-d", &self.data]);
 
-        return match self.curl_command.output() {
+        return match command.output() {
             Ok(o) => Ok(String::from_utf8_lossy(&o.stdout).to_string()),
             Err(e) => Err(e),
         };
     }
 
     pub fn post(&mut self) -> Result<String, io::Error> {
-        self.curl_command.args(["-X", "POST", &self.endpoint, "-d", &self.data]);
+        let mut command = Command::new("curl");
+        command.args(["-X", "POST", &self.endpoint, "-d", &self.data]);
 
-        return match self.curl_command.output() {
+        return match command.output() {
             Ok(o) => Ok(String::from_utf8_lossy(&o.stdout).to_string()),
             Err(e) => Err(e),
         };
