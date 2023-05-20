a173c81e954e6b0360de1552b38d01dc4382b437
André Luis (devkcud) <andrescalisejr@gmail.com>
2023-05-20 18:51:24 -0300

    add curl to requester

--- req/src/request/curl.rs
+++ req/src/request/curl.rs
@@ -1,3 +1,38 @@
-pub struct CURL;
+use std::{io, process::Command};
 
-impl CURL {}
+pub struct CURL {
+    data: String,
+    endpoint: String,
+    curl_command: Command,
+}
+
+impl CURL{
+    pub fn new(endpoint: String, data: String) -> Self {
+        let command = Command::new("curl");
+
+        return Self {
+            data,
+            endpoint,
+            curl_command: command,
+        };
+    }
+
+    pub fn get(&mut self) -> Result<String, io::Error> {
+        self.curl_command.args(["-X", "GET", &self.endpoint, "-d", &self.data]);
+
+        return match self.curl_command.output() {
+            Ok(o) => Ok(String::from_utf8_lossy(&o.stdout).to_string()),
+            Err(e) => Err(e),
+        };
+    }
+
+    pub fn post(&mut self) -> Result<String, io::Error> {
+        self.curl_command.args(["-X", "POST", &self.endpoint, "-d", &self.data]);
+
+        return match self.curl_command.output() {
+            Ok(o) => Ok(String::from_utf8_lossy(&o.stdout).to_string()),
+            Err(e) => Err(e),
+        };
+    }
+
+}
