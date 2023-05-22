c2f53ea0d6a0cdebdd1ad94d2737a9c716777e06
André Luis (devkcud) <andrescalisejr@gmail.com>
2023-05-22 09:50:40 -0300

    add simple logging
    Still not saving to a file

--- req/src/logger/logger.rs
+++ req/src/logger/logger.rs
@@ -1,3 +1,39 @@
-pub struct Logger;
+const LOG_TYPES: [&str; 3] = [
+    "LOG",
+    "WARN",
+    "ERROR",
+];
 
-impl Logger {}
+#[derive(num_enum::IntoPrimitive)]
+#[repr(usize)]
+pub enum LogType {
+    Log,
+    Warn,
+    Error,
+}
+
+pub struct Logger {
+    log_dir: Option<String>,
+    debug: bool,
+}
+
+impl Logger {
+    pub fn new() -> Self {
+        return Self { log_dir: None, debug: false };
+    }
+
+    pub fn set_debug(&mut self, to: Option<bool>) {
+        self.debug = to.unwrap_or(!self.debug);
+    }
+
+    pub fn set_log_dir(&mut self, path: &str) {
+        // TODO: Check for path
+        self.log_dir = Some(path.to_string());
+    }
+
+    pub fn log(self, log_type: LogType, text: String) {
+        if self.debug {
+            println!("{} » {}", LOG_TYPES.get(log_type.try_into().unwrap_or(0)).unwrap(), text);
+        }
+    }
+}
